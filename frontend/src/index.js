1dcbcf107556b9b124910b6c9ac128e4d1db4731
ELREKO <rene.kost@gmail.com>
2025-05-02 11:50:05 +0200

    ds

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/global.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Hallo Welt! React funktioniert!</h1>
   </React.StrictMode>
 );
