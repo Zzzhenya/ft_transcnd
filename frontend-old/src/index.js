213e1c2415939a91d8b582f330a628a00a7168d4
ELREKO <rene.kost@gmail.com>
2025-05-02 11:46:51 +0200

    init

--- frontend-old/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Hallo Welt! React funktioniert!</h1>
-  </React.StrictMode>
-);
