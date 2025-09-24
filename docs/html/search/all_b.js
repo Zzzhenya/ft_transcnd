f77093d05bcca549c28bab50b809fcfe95277e69
Irene Rivero <irene.rivero.casal@gmail.com>
2025-09-24 13:21:57 +0200

    add .gitignore

--- docs/html/search/all_b.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['register_2ecss_0',['Register.css',['../Register_8css.html',1,'']]],
-  ['register_2ejs_1',['Register.js',['../Register_8js.html',1,'']]],
-  ['routes_2ejs_2',['routes.js',['../routes_8js.html',1,'']]]
-];
--- docs/html/search/files_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['footer_2ejs_0',['Footer.js',['../Footer_8js.html',1,'']]]
-];
