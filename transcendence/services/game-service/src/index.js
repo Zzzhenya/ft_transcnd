ca5d2b9f70ddcd40bf9cd16d168eca9a5ff54e7c
Irene Rivero <irene.rivero.casal@gmail.com>
2025-09-30 10:42:16 +0200

    fix: correct ports on each service health checks

--- transcendence/services/game-service/src/index.js
+++ transcendence/services/game-service/src/index.js
@@ -6,8 +6,8 @@ fastify.get('/health', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('game-service running on port 3000');
+    await fastify.listen({ port: 3002, host: '0.0.0.0' });
+    console.log('game-service running on port 3002');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
