298cc30dd4c629371f426dab1d5e6e0db853cb70
Zzzhenya <shenya44@outlook.com>
2025-10-08 22:57:02 +0200

    chore: expose extra health routes

--- transcendence/services/gateway/src/routes/health.route.ts
+++ transcendence/services/gateway/src/routes/health.route.ts
@@ -8,47 +8,47 @@ const healthRoutes: FastifyPluginAsync = async (fastify) => {
 	  return { service: 'gateway', status: 'healthy', timestamp: new Date() };
 	});
 
-// // route:/game-service for game-service
-//     fastify.get('/game-service/health', async (request , reply) => {
-//         try
-//         {
-//             fastify.log.error("Gateway received GET request for /game-service")
-//             const response = await fetch('http://game-service:3002/health', {
-//             method: 'GET',
-//             headers: {
-//             'Authorization': request.headers['authorization'] || '',
-//         }})
-//         const data = await response.json();
-//         reply.status(response.status).send(data);
-//         }
-//         catch (error) {
-//             fastify.log.error(error)
-//             reply.status(404);
-//     }
-//     // reply.code(200).header('Content-Type', 'application/json; charset=utf-8')
-//     // return { hello: 'Users' }
-//   })
+// route:/game-service for game-service
+    fastify.get('/game-service/health', async (request , reply) => {
+        try
+        {
+            fastify.log.error("Gateway received GET request for /game-service")
+            const response = await fetch('http://game-service:3002/health', {
+            method: 'GET',
+            headers: {
+            'Authorization': request.headers['authorization'] || '',
+        }})
+        const data = await response.json();
+        reply.status(response.status).send(data);
+        }
+        catch (error) {
+            fastify.log.error(error)
+            reply.status(404);
+    }
+    // reply.code(200).header('Content-Type', 'application/json; charset=utf-8')
+    // return { hello: 'Users' }
+  })
 
-// // route:/game-service for game-service
-//     fastify.get('/log-service/health', async (request , reply) => {
-//         try
-//         {
-//             fastify.log.error("Gateway received GET request for /log-service")
-//             const response = await fetch('http://log-service:3003/health', {
-//             method: 'GET',
-//             headers: {
-//             'Authorization': request.headers['authorization'] || '',
-//         }})
-//         const data = await response.json();
-//         reply.status(response.status).send(data);
-//         }
-//         catch (error) {
-//             fastify.log.error(error)
-//             reply.status(404);
-//     }
-//     // reply.code(200).header('Content-Type', 'application/json; charset=utf-8')
-//     // return { hello: 'Users' }
-//   })
+// route:/game-service for game-service
+    fastify.get('/log-service/health', async (request , reply) => {
+        try
+        {
+            fastify.log.error("Gateway received GET request for /log-service")
+            const response = await fetch('http://log-service:3003/health', {
+            method: 'GET',
+            headers: {
+            'Authorization': request.headers['authorization'] || '',
+        }})
+        const data = await response.json();
+        reply.status(response.status).send(data);
+        }
+        catch (error) {
+            fastify.log.error(error)
+            reply.status(404);
+    }
+    // reply.code(200).header('Content-Type', 'application/json; charset=utf-8')
+    // return { hello: 'Users' }
+  })
 
 
 }
