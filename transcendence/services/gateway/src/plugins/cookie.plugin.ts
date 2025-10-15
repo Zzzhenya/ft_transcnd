e59c7ffd602f8ee994247fd7418d5fe1138656d4
Zzzhenya <shenya44@outlook.com>
2025-10-15 21:30:32 +0200

    feature: sessionId generated as a cookie

---
+++ transcendence/services/gateway/src/plugins/cookie.plugin.ts
@@ -0,0 +1,11 @@
+import type { FastifyPluginCallback } from 'fastify';
+import cookie from '@fastify/cookie';
+
+const cookiePlugin: FastifyPluginCallback = (fastify, opts, done) => {
+  // fastify.register(cookie, {
+  //   // secret: 'my-secret-key', // Optional (for signed cookies)
+  // });
+  // done();
+};
+
+export default cookiePlugin;
