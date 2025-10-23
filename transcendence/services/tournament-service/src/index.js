ea2549d1e44af3ff60c9a94b476dade05b4d8ee1
Zzzhenya <shenya44@outlook.com>
2025-10-23 20:50:52 +0200

    fix: health working

--- transcendence/services/tournament-service/src/index.js
+++ transcendence/services/tournament-service/src/index.js
@@ -1,29 +1,39 @@
 
-import fastify from 'fastify';
+import Fastify from 'fastify';
 import websocket from '@fastify/websocket';
-import cors from '@fastify/cors';   
-import { broadcastTournamentUpdate } from '../tournament/broadcast.js';
-import { registergetPlayerList } from './tournamentRoute.js';
-import { registeradvanceWinner } from './tournamentRoute.js';
-import { registergetBracket } from './tournamentRoute.js';  
-import { registergenerateBracket } from './tournamentRoute.js';
-import { registerstartserver } from './createTournament.js';
-import { registerhealthRoute } from './healthRoute.js';
-import { registertournamentStatsRoute } from './tournamentStatsRoute.js';
+// import cors from '@fastify/cors';   
+// import { broadcastTournamentUpdate } from './tournament/broadcast.js';
+// import { registergetPlayerList } from './route/tournamentRoute.js';
+// import { registeradvanceWinner } from './route/tournamentRoute.js';
+// import { registergetBracket } from './route/tournamentRoute.js';  
+// import { registergenerateBracket } from './tournament/createBracket.js';
+// import { registercreateTournamentService } from './tournament/createTournament.js';
+import { healthRoute } from './route/healthRoute.js';
+// import { registerhealthRoute } from './route/healthRoute.js';
+// import { registertournamentStatsRoute } from './route/tournamentStats.js';
 
-const fastify = fastify({ logger: true });
-await fastify.register(websocket);
+const fastify = Fastify({ logger: true });
+// await fastify.register(websocket);
 
-const tournaments = new Map();
-let nextTournamentId = 1;
+// const tournaments = new Map();
+// let nextTournamentId = 1;
 
-registerhealthRoute(fastify);
-registerstartserver(fastify, tournaments, () => nextTournamentId++); 
-registergetPlayerList(fastify, tournaments);   
-registergenerateBracket(fastify, tournaments, () => nextTournamentId++);
-registergetBracket(fastify, tournaments);
-registeradvanceWinner(fastify, tournaments, broadcastTournamentUpdate);
-registerDemoRoutes(fastify, tournaments);
-registertournamentStatsRoute(fastify, tournaments);
+// registerhealthRoute(fastify);
+fastify.register(healthRoute);
+// registercreateTournamentService(fastify, tournaments, () => nextTournamentId++);
+// registergetPlayerList(fastify, tournaments);   
+// registergenerateBracket(fastify, tournaments, () => nextTournamentId++);
+// registergetBracket(fastify, tournaments);
+// registeradvanceWinner(fastify, tournaments, broadcastTournamentUpdate);
+// registerDemoRoutes(fastify, tournaments);
+// registertournamentStatsRoute(fastify, tournaments);
+
+fastify.listen({ port: 3005, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`Tournament service listening at ${address}`);
+});
 
 
