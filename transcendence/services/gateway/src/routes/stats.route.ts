677baac36a4e7eee2c39e211a708a9d63a759e3f
Zzzhenya <shenya44@outlook.com>
2025-10-25 20:06:12 +0200

    chore: refactor stats

--- transcendence/services/gateway/src/routes/stats.route.ts
+++ transcendence/services/gateway/src/routes/stats.route.ts
@@ -5,26 +5,7 @@ import logger from '../utils/logger.js'; // log-service
 const statsRoutes: FastifyPluginAsync = async (fastify) => {
 
 	fastify.get('/user-service/stats', async (request, reply) => {
-	try
-	{
-	    fastify.log.error("Gateway received GET request for /game-service")
-	    const response = await fetch('http://game-service:3002/stats', {
-	    method: 'GET',
-	    headers: {
-	    'Authorization': request.headers['authorization'] || '',
-	}})
-	const data = await response.json();
-	reply.status(response.status).send(data);
-	}
-    catch (error: any) {
-        logger.error('[[Gateway]] GET request for /user-service/stats failed', error);
-        fastify.log.error(error);
-        return gatewayError(
-        	reply,
-        	503,
-        	'Service Unavailable',
-        	'The upstream service is currently unavailable.');
-    }
+		return proxyRequest(fastify, request, reply, 'http://game-service:3002/stats', 'GET');
 	});
 
 };
