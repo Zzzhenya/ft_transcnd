cb4728a15727eb4acffb78627a6082e05eb72b0f
emily-cloud <emilyaiai86@gmail.com>
2025-11-08 17:17:29 +0100

    fix: delete frontend_jason folder

--- frontend_jason/src/app/auth.ts
+++
@@ -1,5 +0,0 @@
-export type { AuthUser } from "./mock-auth";
-export { getAuth, mockSignIn as signIn, mockSignOut as signOut } from "./mock-auth";
-
-// When converting from mock-auth to real-auth:
-// export { getAuth, signIn, signOut } from "./real-auth";
