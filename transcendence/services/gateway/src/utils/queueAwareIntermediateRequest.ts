1841f618b0ac7aefa5fc8400a1cf9dc0a308b52b
Zzzhenya <shenya44@outlook.com>
2025-11-13 18:23:11 +0100

    feat: remove cookie adding in requeust handlers

--- transcendence/services/gateway/src/utils/queueAwareIntermediateRequest.ts
+++ transcendence/services/gateway/src/utils/queueAwareIntermediateRequest.ts
@@ -137,19 +137,19 @@ export async function queueAwareIntermediateRequest(
       };
     }
 
-    reply.setCookie('token', data.token, {
-          httpOnly: true,
-          secure: true,           // ✅ Only HTTPS for production
-          sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
-          path: '/',              // ✅ Valid across all routes
-        })  
+    // reply.setCookie('token', data.token, {
+    //       httpOnly: true,
+    //       secure: true,           // ✅ Only HTTPS for production
+    //       sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
+    //       path: '/',              // ✅ Valid across all routes
+    //     })  
 
-    reply.setCookie('sessionId', data.sessionId, {
-          httpOnly: true,
-          secure: true,           // ✅ Only HTTPS for production
-          sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
-          path: '/',              // ✅ Valid across all routes
-        }) 
+    // reply.setCookie('sessionId', data.sessionId, {
+    //       httpOnly: true,
+    //       secure: true,           // ✅ Only HTTPS for production
+    //       sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
+    //       path: '/',              // ✅ Valid across all routes
+    //     }) 
     // For successful responses, set the status code
     reply.status(response.status);
     // return data;
--- transcendence/services/gateway/src/utils/queueAwareProxyHandler.ts
+++ transcendence/services/gateway/src/utils/queueAwareProxyHandler.ts
@@ -144,19 +144,19 @@ export async function queueAwareProxyRequest(
 
     const data = await response.json();
 
-    reply.setCookie('token', data.token, {
-      httpOnly: true,
-      secure: true,           // ✅ Only HTTPS for production
-      sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
-      path: '/',              // ✅ Valid across all routes
-    })
+    // reply.setCookie('token', data.token, {
+    //   httpOnly: true,
+    //   secure: true,           // ✅ Only HTTPS for production
+    //   sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
+    //   path: '/',              // ✅ Valid across all routes
+    // })
 
-    reply.setCookie('sessionId', data.sessionId, {
-      httpOnly: true,
-      secure: true,           // ✅ Only HTTPS for production
-      sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
-      path: '/',              // ✅ Valid across all routes
-    })
+    // reply.setCookie('sessionId', data.sessionId, {
+    //   httpOnly: true,
+    //   secure: true,           // ✅ Only HTTPS for production
+    //   sameSite: 'lax',       // ✅ Required for cross-origin if frontend is on another domain
+    //   path: '/',              // ✅ Valid across all routes
+    // })
 
     return reply.status(response.status).send(data);
 
