d15ca619b483748d01e571526d8a5aef490ff614
Shenya <shenya44@outlook.com>
2025-11-24 19:03:09 +0100

    chore: remove token from body after setting the cookie

--- transcendence/services/gateway/src/utils/proxyHandler.ts
+++ transcendence/services/gateway/src/utils/proxyHandler.ts
@@ -116,7 +116,7 @@ export async function proxyRequest(
         path: '/',
       });
       // remove token from the body
-      // delete data.token;
+      delete data.token;
     }
     return reply.send(data);
 
